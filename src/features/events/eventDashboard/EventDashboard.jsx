f33988b782becdbc63ee905c2907af3f7780f637
your name <your e-mail>
2021-03-26 14:43:56 +0300

    End of section 4

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -4,18 +4,39 @@ import EventForm from '../eventForm/EventForm';
 import EventList from './EventList';
 import {sampleData} from '../../../app/api/sampleData.js'
 
-const EventDashboard = ({formOpen,setFormOpen}) => {
+const EventDashboard = ({formOpen,setFormOpen, selectEvent,selectedEvent}) => {
     const [events, setEvents] = useState(sampleData);
+
+      function handleCreateEvent (event) {
+        setEvents([...events,event])
+    }
+
+    function handleUpdateEvent (updatedEvent) {
+        setEvents(events.map(evt => evt.id === updatedEvent.id ? updatedEvent : evt));
+        selectEvent(null);
+    }
+    function handleDeleteEvent (eventID) {
+        setEvents(events.filter(evt => evt.id !== eventID ));
+    }
+
+
     
 
     return (
     <Grid>
         <GridColumn width={10}>
-        <EventList events={events} />
+        <EventList events={events} selectEvent={selectEvent} deleteEvent={handleDeleteEvent}/>
         </GridColumn>
         <GridColumn width={6}>
             {formOpen &&
-            <EventForm setFormOpen={setFormOpen}/>}
+            <EventForm 
+             setFormOpen={setFormOpen}
+             setEvents={setEvents} 
+             createEvent={handleCreateEvent}
+             selectedEvent={selectedEvent}
+             key={selectedEvent ? selectedEvent.id:null}
+             updateEvent={handleUpdateEvent}
+              />}
         </GridColumn>
     </Grid>
     );
